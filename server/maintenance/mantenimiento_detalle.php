23bbef4b483df77c28a896687409d9420fef8fd0
mairon754 <maironandres754@gmail.com>
2025-05-20 17:03:13 -0500

    orientado a la web

--- server/maintenance/mantenimiento_detalle.php
+++ server/maintenance/mantenimiento_detalle.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // Usar una ruta fija desde la raíz del proyecto
-require_once 'Location: ../../db/db.php';
+require_once '../../db/db.php';
 
 // Verificar si el usuario está logueado
 if (!isset($_SESSION['user_id'])) {
