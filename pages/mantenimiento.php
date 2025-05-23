f0192b64fa43f6894f898d3c4c3e3f88dfe817b3
mairon754 <maironandres754@gmail.com>
2025-05-22 21:16:41 -0500

    reforma en los estilos y navbar 2

--- pages/mantenimiento.php
+++ pages/mantenimiento.php
@@ -72,9 +72,9 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mantenimiento</title>
-    <link rel="stylesheet" href="../css/pages.css">
-    <link rel="stylesheet" href="css/navbar-global.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+<link rel="stylesheet" href="../css/pages.css">
+<link rel="stylesheet" href="css/navbar-global.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
 
     </style>
